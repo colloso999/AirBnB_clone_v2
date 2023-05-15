30368c3d0ce9f128cb830b82ce6b476cda2704c9
colloso999 <tripperskripper@gmail.com>
2023-05-15 21:31:32 +0000

    a Fabric script that generates a .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@ from fabric.api import local
 from time import strftime
 from datetime import date
 
+
 def do_pack():
     """
     A script that generates archive the contents of web_static folder
@@ -11,7 +12,7 @@ def do_pack():
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-                .format(filename))
+              .format(filename))
         return "versions/web_static_{}.tgz".format(filename)
     except Exception as e:
         return None
