94d4a334d9e82750307588fbf70a4ae68ed8af3b
colloso999 <tripperskripper@gmail.com>
2023-06-01 10:33:04 +0300

    users model

--- models/user.py
+++ models/user.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import hashlib
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+from os import getenv
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -23,3 +24,16 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd
