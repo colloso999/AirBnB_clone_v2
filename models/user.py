d2fa2b968901b1bbb8991a24a3409544858f0739
colloso999 <tripperskripper@gmail.com>
2023-06-04 13:51:56 +0300

    modified users model

--- models/user.py
+++ models/user.py
@@ -23,17 +23,4 @@ class User(BaseModel, Base):
         email = ""
         password = ""
         first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
+        last_name = "" 
